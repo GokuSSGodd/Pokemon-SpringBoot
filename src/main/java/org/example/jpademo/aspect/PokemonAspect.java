40f675efdc9fa5f8d9a0135dd453de76feb034e8
Murtaza Amjad <murtazaamjad0519@gmail.com>
2025-10-23 19:00:29 -0400

    Learned & Implemented Aspect-Oriented Programming (AOP) using SpringBoot
    Learned about the difference between Before, After, and Around. Learned the @Aspect and more about the @Component method, how to call a log from another class using AOP, how to implement @Pointcut, what JoinPoints and ProceedingJoinPoints are, etc. The PokemonAspect is able to get any method within PokemonController use it and log it to the log file.

--- src/main/java/org/example/jpademo/aspect/PokemonAspect.java
+++ src/main/java/org/example/jpademo/aspect/PokemonAspect.java
@@ -1,4 +1,30 @@
 package org.example.jpademo.aspect;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.security.SignedObject;
+
+@Slf4j
+@Aspect
+@Component
 public class PokemonAspect {
+
+    @Pointcut("execution(* org.example.jpademo.controller.PokemonController.*(..))")
+    private void getPokemonRequest(){}
+
+    @Around("getPokemonRequest()")
+    public Object logGetPokemon(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object methodName = proceedingJoinPoint.getSignature().getName();
+        log.info("Method Started called: {}()",  methodName);
+        Object returnPoint = proceedingJoinPoint.proceed();
+        log.info("Method Ended called: {}()", methodName);
+        return returnPoint;
+    }
 }
--- src/main/java/org/example/jpademo/controller/PokemonController.java
+++ src/main/java/org/example/jpademo/controller/PokemonController.java
@@ -6,8 +6,6 @@ import org.example.jpademo.data.Pokemon;
 import org.example.jpademo.exception.PokemonException;
 import org.example.jpademo.service.PokemonRegionService;
 import org.example.jpademo.service.PokemonService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 public class PokemonController{
     private final PokemonRegionService pokemonRegionService;
     private final PokemonService pokemonService;
+
 
     public PokemonController(PokemonRegionService pokemonRegionService, PokemonService pokemonService) {
         this.pokemonRegionService = pokemonRegionService;
